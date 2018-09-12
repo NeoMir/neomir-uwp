c6b41265ba9bacc293ae131a1a3881a6ec20a936
robin.dacalor <robin.dacalor@gmail.com>
2018-09-12 17:14:39 +0200

    Delete useless Using

--- NeoMir/MainPage.xaml.cs
+++ NeoMir/MainPage.xaml.cs
@@ -2,26 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Composition;
-using Windows.Devices.Geolocation;
-using System.Diagnostics;
 using System.Net.Http;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading;
 using System.Globalization;
-using System.Numerics;
 using Windows.UI.Xaml.Media.Animation;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
