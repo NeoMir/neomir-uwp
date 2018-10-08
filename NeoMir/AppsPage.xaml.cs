a6fce4618108b59991ee78902ae45ca91e401f3d
robin.dacalor <robin.dacalor@gmail.com>
2018-10-08 03:07:56 +0200

    Add new feature - Launch other applications inside the UWP app
    We can use a custom URI for each application, a developer has to provide his URI to let us be able to launch it.

--- NeoMir/AppsPage.xaml.cs
+++ NeoMir/AppsPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
@@ -12,9 +16,86 @@ namespace NeoMir
     /// </summary>
     public sealed partial class AppsPage : Page
     {
+        public static int ImageSize { get; private set; }
+        public static int ImageHoverSize { get; private set; }
+        public static Thickness ImageMargin { get; private set; }
+
         public AppsPage()
         {
             this.InitializeComponent();
+            ImageSize = 350;
+            ImageHoverSize = 330;
+            ImageMargin = new Thickness(10);
+            ListApps();
+        }
+
+        private void ListApps()
+        {
+            int numberOfApps = 20;
+
+            // test for images
+            int numberImage = 0;
+
+            for (int i = 0; i < numberOfApps; i++)
+            {
+                Image img = new Image();
+                if (numberImage == 0)
+                {
+                    img.Source = new BitmapImage(new Uri("ms-appx:///Assets/AppsPage/exemple.png"));
+                    img.Name = "mailto:";
+                    numberImage++;
+                }
+                else if (numberImage == 1)
+                {
+                    img.Source = new BitmapImage(new Uri("ms-appx:///Assets/AppsPage/exemple1.png"));
+                    img.Name = "ms-settings:";
+                    numberImage++;
+                }
+                else if (numberImage == 2)
+                {
+                    img.Source = new BitmapImage(new Uri("ms-appx:///Assets/AppsPage/exemple2.png"));
+                    img.Name = "ms-windows-store://home/";
+                    numberImage++;
+                }
+                else if (numberImage == 3)
+                {
+                    img.Source = new BitmapImage(new Uri("ms-appx:///Assets/AppsPage/exemple3.png"));
+                    img.Name = "bingmaps:";
+                    numberImage = 0;
+                }
+                img.Height = ImageSize;
+                img.Width = ImageSize;
+                img.Margin = ImageMargin;
+                img.PointerEntered += new PointerEventHandler(image_PointerEntered);
+                img.PointerExited += new PointerEventHandler(image_PointerExited);
+                img.Tapped += new TappedEventHandler(image_Tapped);
+                rectangleItems.Items.Add(img);
+            }
+        }
+
+        private void image_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Image img = (Image)sender;
+            img.Height = ImageSize;
+            img.Width = ImageSize;
+        }
+
+        private void image_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            Image img = (Image)sender;
+            img.Height = ImageHoverSize;
+            img.Width = ImageHoverSize;
+        }
+
+        private async void image_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Image img = (Image)sender;
+            img.Height = ImageSize;
+
+            // We can use custom URI of the App that we want to launch
+            string uriString = string.Format(img.Name);
+            Uri uri = new Uri(uriString);
+            await Launcher.LaunchUriAsync(uri);
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
