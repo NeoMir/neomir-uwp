95997796715cb2a3bac8c9b95b8ea3e6ca4f882d
ALTAWAK/m.lumbanzila <m.lumbanzila@scancube.com>
2020-01-05 12:45:14 +0100

    Delivery

--- NeoMir/Globals/GlobalStrings.cs
+++ NeoMir/Globals/GlobalStrings.cs
@@ -8,13 +8,13 @@ namespace NeoMir.Globals
 {
     public static class GlobalStrings
     {
-        public static string PhotoRegistred = "Votre photo est enregistré, vous pouvez maintenant vous connécter via la reconnaissance facial";
-        public static string PhotoNotRegistred = "Veuillez réessayer en placent votre visage au centre du miroir";
+        public static string PhotoRegistred = "Votre photo est enregistrée, vous pouvez maintenant vous connecter via la reconnaissance faciale";
+        public static string PhotoNotRegistred = "Veuillez réessayer en placant votre visage au centre du miroir";
         public static string PhotoTookProcessing = "Traitement de la photo";
         public static string PhotoRequired = "L'enregistrement de votre photo de connexion va commencer ...";
 
-        public static string DoLockToScanFace = "Faites le geste de vérouillage pour commencer la reconnaissance faciale";
-        public static string NoFaceDetected = "Aucune personne reconnu";
+        public static string DoLockToScanFace = "Faites le geste de verrouillage pour commencer la reconnaissance faciale";
+        public static string NoFaceDetected = "Aucune personne reconnue";
         public static string FaceScanningRunning = "Reconnaissance faciale en cours ...";
 
     }
